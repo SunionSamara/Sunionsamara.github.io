e91b050052262e998a32eff35294d28af0b4bcc2
SunionSamara <50754798+SunionSamara@users.noreply.github.com>
2021-11-08 14:04:58 +0300

    Add files via upload

---
+++ builder/scripts/project/scriptsInEvents.js
@@ -0,0 +1,55 @@
+
+
+
+const scriptsInEvents = {
+
+		async EventSheet1_Event116_Act2(runtime, localVars)
+		{
+			const colorPrimary = document.getElementById("color-primary");
+			colorPrimary.type = "color";
+			
+		},
+
+		async EventSheet1_Event117_Act2(runtime, localVars)
+		{
+			const colorPrimary = document.getElementById("color-primary");
+			colorPrimary.type = "color";
+			
+		},
+
+		async EventSheet1_Event119_Act2(runtime, localVars)
+		{
+			const s = localVars.colorStr;
+			runtime.globalVars.r=parseInt("0x"+ s.slice(1,3));
+			runtime.globalVars.g=parseInt("0x"+ s.slice(3,5));
+			runtime.globalVars.b=parseInt("0x"+ s.slice(5,7));
+			
+		},
+
+		async EventSheet1_Event121_Act2(runtime, localVars)
+		{
+			const colorPrimary = document.getElementById("color-primary-grid");
+			colorPrimary.type = "color";
+			
+		},
+
+		async EventSheet1_Event122_Act2(runtime, localVars)
+		{
+			const colorPrimary = document.getElementById("color-primary-grid");
+			colorPrimary.type = "color";
+			
+		},
+
+		async EventSheet1_Event124_Act2(runtime, localVars)
+		{
+			const s = localVars.colorStr;
+			runtime.globalVars.r2=parseInt("0x"+ s.slice(1,3));
+			runtime.globalVars.g2=parseInt("0x"+ s.slice(3,5));
+			runtime.globalVars.b2=parseInt("0x"+ s.slice(5,7));
+			
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
